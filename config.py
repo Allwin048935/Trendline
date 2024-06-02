8cfd956364e46686120162126900785de14c8cb2
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-06-02 15:10:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Binance API credentials
-BINANCE_API_KEY = 'your_api_key'
-BINANCE_API_SECRET = 'your_api_secret'
+BINANCE_API_KEY = 'veiqd07BuRmMlxy3eeRLLKNyDnFrIphTcqgSM7XTRUCzQWTyqxK4sPtfVZioaVHi'
+BINANCE_API_SECRET = 'N4myBDNkkD203gxxpo8NLQgdZxdvzm7N7PpBjBU0DiTtctTLaFQEVev51N4P5R5g'
 
 # Telegram Bot token and chat ID
 TELEGRAM_BOT_TOKEN = 'your_bot_token'
